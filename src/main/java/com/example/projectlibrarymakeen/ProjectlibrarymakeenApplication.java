8dae91be9a2ed81ffbf21efe9b01e152cd10ea5e
neda <saeid@gmail.com>
2024-01-04 11:42:42 +0330

    Initial commit

---
+++ src/main/java/com/example/projectlibrarymakeen/ProjectlibrarymakeenApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectlibrarymakeen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectlibrarymakeenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectlibrarymakeenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectlibrarymakeen/ProjectlibrarymakeenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectlibrarymakeen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectlibrarymakeenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
